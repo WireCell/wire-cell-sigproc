2ef3536cb8b649b991cfeb9354fd33779304acc8
Xin Qian <last.qx@gmail.com>
2016-09-29 20:36:49 -0400

    fix a bug about misconfigured channels

--- apps/exam_noise_filters.cxx
+++ apps/exam_noise_filters.cxx
@@ -270,9 +270,9 @@ int main(int argc, char* argv[])
     vector<int> miscfgchan;
     const double from_gain_mVfC=7.8, to_gain_mVfC=14.0,
 	from_shaping=1.0*units::microsecond, to_shaping=2.0*units::microsecond;
-    for (int ind=2016; ind<= 2096; ++ind) { miscfgchan.push_back(ind); }
+    for (int ind=2016; ind<= 2095; ++ind) { miscfgchan.push_back(ind); }
     for (int ind=2192; ind<= 2303; ++ind) { miscfgchan.push_back(ind); }
-    for (int ind=2352; ind<= 2400; ++ind) { miscfgchan.push_back(ind); }
+    for (int ind=2352; ind< 2400; ++ind) { miscfgchan.push_back(ind); }
     
     // hard-coded bad channels
     vector<int> bad_channels;
