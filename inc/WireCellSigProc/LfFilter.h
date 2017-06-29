c87778658d3fe1832c76b496e422db43b84e3fb9
Xin Qian <last.qx@gmail.com>
2017-06-28 22:28:57 -0400

    add many filters

---
+++ inc/WireCellSigProc/LfFilter.h
@@ -0,0 +1,35 @@
+
+#ifndef WIRECELLSIGPROC_LFFILTER
+#define WIRECELLSIGPROC_LFFILTER
+
+#include "WireCellIface/IFilterWaveform.h"
+#include "WireCellIface/IConfigurable.h"
+#include "WireCellUtil/Response.h"
+
+namespace WireCell {
+  namespace SigProc {
+    class LfFilter : public IFilterWaveform, public IConfigurable {
+    public :
+      LfFilter(int nbins = 100, double max_freq = 1 * units::megahertz, double tau = 0.02* units::megahertz);
+      virtual ~LfFilter();
+
+      virtual const Waveform::realseq_t filter_waveform() const ;
+
+      // IConfigurable
+      virtual void configure(const WireCell::Configuration& config);
+      virtual WireCell::Configuration default_configuration() const;
+      
+      
+    private:
+      // number of bins ...
+      int m_nbins;
+      double m_max_freq;
+      double m_tau;
+
+      
+      
+    };
+  }
+}
+
+#endif
