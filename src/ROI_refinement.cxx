008d69dc77cce401e6d2151041f891e2b77b343f
Xin Qian <last.qx@gmail.com>
2017-07-02 08:30:19 -0400

    add class for ROI refiement

---
+++ src/ROI_refinement.cxx
@@ -0,0 +1,13 @@
+#include "ROI_refinement.h"
+#include <iostream>
+
+using namespace WireCell;
+using namespace WireCell::SigProc;
+
+ROI_refinement::ROI_refinement()
+{
+  
+}
+
+ROI_refinement::~ROI_refinement(){
+}
---
+++ src/ROI_refinement.h
@@ -0,0 +1,19 @@
+#ifndef WIRECELLSIGPROC_ROIREFINEMENT
+#define WIRECELLSIGPROC_ROIREFINEMENT
+
+#include <vector>
+#include <map>
+
+namespace WireCell{
+  namespace SigProc{
+    class ROI_refinement{
+    public:
+      ROI_refinement();
+      ~ROI_refinement();
+      
+    private:
+      
+    };
+  }
+}
+#endif
