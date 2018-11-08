046607e0b76fb60038c8a96bb0b81469f613a073
Hanyu Wei <weihy@lycastus.phy.bnl.gov>
2018-11-08 17:51:00 -0500

    update DBChannelSelector

--- inc/WireCellSigProc/DBChannelSelector.h
+++ inc/WireCellSigProc/DBChannelSelector.h
@@ -8,6 +8,7 @@
 
 #include "WireCellIface/IFrameFilter.h"
 #include "WireCellIface/IConfigurable.h"
+#include "WireCellIface/IChannelNoiseDatabase.h"
 #include "WireCellSigProc/ChannelSelector.h"
 
 #include <string>
@@ -22,10 +23,16 @@ namespace WireCell {
             DBChannelSelector();
             virtual ~DBChannelSelector();
 
+        /// IFrameFilter interface.
+        bool operator()(const input_pointer& in, output_pointer& out);
+
 	    /// IConfigurable interface.
 	    void configure(const WireCell::Configuration& config);
 	    WireCell::Configuration default_configuration() const;
-            
+        
+        private:
+            std::string m_type;
+            WireCell::IChannelNoiseDatabase::pointer m_db;
         };
     }
 }
--- src/DBChannelSelector.cxx
+++ src/DBChannelSelector.cxx
@@ -35,23 +35,29 @@ WireCell::Configuration DBChannelSelector::default_configuration() const
 
 void DBChannelSelector::configure(const WireCell::Configuration& cfg)
 {
-    std::string type = get<std::string>(cfg, "type", "misconfigured");
+    m_type = get<std::string>(cfg, "type", "misconfigured");
 
     // channels from database
     auto jcndb = cfg["channelDB"];
     if (!jcndb.empty()) {
-        auto db = Factory::find_tn<IChannelNoiseDatabase>(jcndb.asString());
+        m_db = Factory::find_tn<IChannelNoiseDatabase>(jcndb.asString());
         std::cerr <<"DBChannelSelector: using channel database object: "
-            << " \"" << jcndb.asString() << "\" type: \"" << type << "\"\n";
-        if( type == "bad" ) ChannelSelector::set_channels(db->bad_channels());
-        if( type == "misconfigured" ) ChannelSelector::set_channels(db->miscfg_channels()); 
-        //std::vector<int> miscfg_channels = db->miscfg_channels(); 
-        //std::cout << "miscfg channel size: " << miscfg_channels.size() <<"\n";
-        //for(size_t i=0; i< miscfg_channels.size(); i++) {
-        //std::cout << "miscfg channel: " << miscfg_channels.at(i) <<"\n";
-        //}
+            << " \"" << jcndb.asString() << "\" type: \"" << m_type << "\"\n";
     }
     else{
         THROW(ValueError() << errmsg{"DBChannelSelector: no database configured"});
     }
+}
+
+bool DBChannelSelector::operator()(const input_pointer& in, output_pointer& out)
+{
+    if( m_type == "bad" ) ChannelSelector::set_channels(m_db->bad_channels());
+    if( m_type == "misconfigured" ) ChannelSelector::set_channels(m_db->miscfg_channels()); 
+    //std::vector<int> miscfg_channels = db->miscfg_channels(); 
+    //std::cout << "miscfg channel size: " << miscfg_channels.size() <<"\n";
+    //for(size_t i=0; i< miscfg_channels.size(); i++) {
+    //std::cout << "miscfg channel: " << miscfg_channels.at(i) <<"\n";
+    //}
+    
+    return ChannelSelector::operator()(in, out);
 }
