361e4a0cba2b9ca5e3c439621152fb03dc8df064
Xin Qian <last.qx@gmail.com>
2018-12-08 19:28:39 -0500

    update

--- src/Microboone.cxx
+++ src/Microboone.cxx
@@ -293,7 +293,9 @@ std::vector< std::vector<int> > Microboone::SignalProtection(WireCell::Waveform:
 	    }
 	}
     }
-  
+
+    //std::cout << "Xin: " << respec.size() << " " << res_offset << std::endl;
+    
     // the deconvolution protection code ... 
     if (respec.size() > 0 && (respec.at(0).real()!=1 || respec.at(0).imag()!=0) && res_offset!=0){
 	//std::cout << nbin << std::endl;
@@ -848,6 +850,13 @@ Microboone::CoherentNoiseSub::apply(channel_signals_t& chansig) const
 
     // do the signal protection and adaptive baseline
     std::vector< std::vector<int> > rois = Microboone::SignalProtection(medians,respec,res_offset,pad_f,pad_b,decon_limit, adc_limit);
+
+    // if (achannel == 3840){
+    // 	std::cout << "Xin1: " << rois.size() << std::endl;
+    // 	for (size_t i=0;i!=rois.size();i++){
+    // 	    std::cout << "Xin1: " << rois.at(i).front() << " " << rois.at(i).back() << std::endl;
+    // 	}
+    // }
     
     //std::cerr <<"\tSigprotection done: " << chansig.size() << " " << medians.size() << " " << medians.at(100) << " " << medians.at(101) << std::endl;
 
